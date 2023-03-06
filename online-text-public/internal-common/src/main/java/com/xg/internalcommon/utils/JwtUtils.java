de8d3115e91659bb1e1e1eb06bb9f784ce9e48f5
LAPTOP-1OOE5B78\junxuan <747304666@qq.com>
2023-03-06 20:43:03 +0800

    解析token

--- online-text-public/internal-common/src/main/java/com/xg/internalcommon/utils/JwtUtils.java
+++ online-text-public/internal-common/src/main/java/com/xg/internalcommon/utils/JwtUtils.java
@@ -4,6 +4,8 @@ package com.xg.internalcommon.utils;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -15,11 +17,13 @@ import java.util.Map;
  */
 public class JwtUtils {
     //盐
-    private  static final String SIGN="XGLOVEXIAODAN!!@";
-
+    private static final String SIGN="XGLOVEXIAODAN!!@";
+    private static final String JWT_KEY="passengerPhone";
     //生成token
-    public static String generatorToken(Map<String,String> map){
+    public static String generatorToken(String passengerPhone){
         //定义token过期时间
+        Map<String,String> map=new HashMap<>();
+        map.put(JWT_KEY,passengerPhone);
         Calendar calendar=Calendar.getInstance();
         calendar.add(Calendar.DATE,1);
         Date date=calendar.getTime();
@@ -33,13 +37,18 @@ public class JwtUtils {
         return sign;
     }
 
-    public static void main(String[] args) {
-        Map<String,String> map=new HashMap<>();
-        map.put("name","zhangsan");
-        map.put("age","18");
-        String s = generatorToken(map);
-        System.out.println("生成的token:"+s);
+    //解析token
+    public static String parseToken(String token){
+        DecodedJWT verify = JWT.require(Algorithm.HMAC256(SIGN)).build().verify(token);
+        Claim passengerPhone = verify.getClaim(JWT_KEY);
+        return passengerPhone.toString();
     }
 
+    public static void main(String[] args) {
+        String s = generatorToken("15666015992");
+        System.out.println("生成的token:\n"+s);
+        String s1 = parseToken(s);
+        System.out.println("解析的 token: \n"+s1);
+    }
 
 }
