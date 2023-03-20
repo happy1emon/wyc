ce5e980f989312c0ef956a4eeec251f0f0df281e
LAPTOP-1OOE5B78\junxuan <747304666@qq.com>
2023-03-20 22:24:29 +0800

    no message

--- online-text-public/api-passenger/src/main/java/com/xg/apipassenger/controller/ForecastPriceController.java
+++ online-text-public/api-passenger/src/main/java/com/xg/apipassenger/controller/ForecastPriceController.java
@@ -8,8 +8,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 @RestController
 @Slf4j
@@ -25,12 +27,10 @@ public class ForecastPriceController {
         log.info("目的地的经度：" + forecastPriceDTO.getDestLongitude());
         log.info("目的地的纬度：" + forecastPriceDTO.getDestLatitude());
         log.info("调用计价服务，计算价格");
-
         String depLongtitude = forecastPriceDTO.getDepLongitude();
         String depLatitude = forecastPriceDTO.getDepLatitude();
         String destLongtitude = forecastPriceDTO.getDestLongitude();
         String destLatitude = forecastPriceDTO.getDestLatitude();
-
         return forecastPriceService.forecastPrice(depLongtitude,
                 depLatitude, destLongtitude, destLatitude);
     }
